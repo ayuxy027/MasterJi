a98157ceee7d633b4d841917b1952a0d999f3ab7
Ayush <ayush421301@gmail.com>
2025-10-31 19:49:05 +0530

    fix: remove glow bg from hero section and replaced it with pattern

--- src/design/Pattern.tsx
+++ src/design/Pattern.tsx
@@ -7,7 +7,7 @@ interface PatternProps {
 const Pattern: React.FC<PatternProps> = ({ children }) => {
   return (
     <div className="min-h-screen w-full relative">
-      {/* Dashed Top Right Fade Grid */}
+      {/* Dashed Grid */}
       <div
         className="absolute inset-0 z-0"
         style={{
@@ -18,38 +18,36 @@ const Pattern: React.FC<PatternProps> = ({ children }) => {
           backgroundSize: "20px 20px",
           backgroundPosition: "0 0, 0 0",
           maskImage: `
-              repeating-linear-gradient(
-                  to right,
-                  black 0px,
-                  black 3px,
-                  transparent 3px,
-                  transparent 8px
-                ),
-                repeating-linear-gradient(
-                  to bottom,
-                  black 0px,
-                  black 3px,
-                  transparent 3px,
-                  transparent 8px
-                ),
-                radial-gradient(ellipse 80% 80% at 100% 0%, #000 50%, transparent 90%)
+            repeating-linear-gradient(
+              to right,
+              black 0px,
+              black 3px,
+              transparent 3px,
+              transparent 8px
+            ),
+            repeating-linear-gradient(
+              to bottom,
+              black 0px,
+              black 3px,
+              transparent 3px,
+              transparent 8px
+            )
           `,
           WebkitMaskImage: `
-        repeating-linear-gradient(
-                  to right,
-                  black 0px,
-                  black 3px,
-                  transparent 3px,
-                  transparent 8px
-                ),
-                repeating-linear-gradient(
-                  to bottom,
-                  black 0px,
-                  black 3px,
-                  transparent 3px,
-                  transparent 8px
-                ),
-                radial-gradient(ellipse 80% 80% at 100% 0%, #000 50%, transparent 90%)
+            repeating-linear-gradient(
+              to right,
+              black 0px,
+              black 3px,
+              transparent 3px,
+              transparent 8px
+            ),
+            repeating-linear-gradient(
+              to bottom,
+              black 0px,
+              black 3px,
+              transparent 3px,
+              transparent 8px
+            )
           `,
           maskComposite: "intersect",
           WebkitMaskComposite: "source-in",
